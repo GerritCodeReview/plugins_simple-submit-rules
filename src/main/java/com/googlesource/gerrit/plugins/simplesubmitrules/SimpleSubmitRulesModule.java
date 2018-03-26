e1b3c48d16ffe492281669258c1e67fbce1f0f2f
Maxime Guerreiro <maximeg@google.com>
2018-03-26 15:15:13 +0200

    Add first version of the simple-submit-rules plugin
    Add a REST API to configure the rules and labels.
    Add the "Unresolved Comments" rule.
    Add the "Require non Author Approval" rule.
    Add tests, documentation and required bootstrap files (BUILD).
    
    Change-Id: I0167ffa57e27b10d5e977ef2684f20759531ccc6

---
+++ src/main/java/com/googlesource/gerrit/plugins/simplesubmitrules/SimpleSubmitRulesModule.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.simplesubmitrules;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.gerrit.server.config.PluginConfig;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.gerrit.server.project.ProjectResource;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.rules.SubmitRule;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.simplesubmitrules.config.ConfigServlet;
+import com.googlesource.gerrit.plugins.simplesubmitrules.config.ConfigTranslator;
+import com.googlesource.gerrit.plugins.simplesubmitrules.rules.NoUnresolvedCommentsRule;
+import com.googlesource.gerrit.plugins.simplesubmitrules.rules.RequireNonAuthorApprovalRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleSubmitRulesModule extends AbstractModule {
+  private static final Logger log = LoggerFactory.getLogger(SimpleSubmitRulesModule.class);
+  private final PluginConfigFactory pluginConfigFactory;
+
+  @Inject
+  public SimpleSubmitRulesModule(PluginConfigFactory pluginConfigFactory) {
+    this.pluginConfigFactory = pluginConfigFactory;
+  }
+
+  @Override
+  protected void configure() {
+    bind(ConfigTranslator.class);
+
+    install(
+        new RestApiModule() {
+          @Override
+          protected void configure() {
+            get(ProjectResource.PROJECT_KIND, Constants.API_ENDPOINT).to(ConfigServlet.class);
+            put(ProjectResource.PROJECT_KIND, Constants.API_ENDPOINT).to(ConfigServlet.class);
+          }
+        });
+
+    DynamicSet.bind(binder(), SubmitRule.class).to(RequireNonAuthorApprovalRule.class);
+    DynamicSet.bind(binder(), SubmitRule.class).to(NoUnresolvedCommentsRule.class);
+  }
+
+  public PluginConfig getConfig(ChangeData changeData) {
+    try {
+      return pluginConfigFactory.getFromProjectConfig(changeData.project(), Constants.PLUGIN_NAME);
+    } catch (NoSuchProjectException e) {
+      log.error("Could not load plugin configuration", e);
+      return null;
+    }
+  }
+}
---
+++ src/main/java/com/googlesource/gerrit/plugins/simplesubmitrules/config/ConfigTranslator.java
@@ -0,0 +1,146 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.simplesubmitrules.config;
+
+import com.google.gerrit.common.data.LabelType;
+import com.google.gerrit.common.data.LabelTypes;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.server.config.PluginConfig;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.project.ProjectState;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.simplesubmitrules.Constants;
+import com.googlesource.gerrit.plugins.simplesubmitrules.config.SubmitConfig.CommentsRules;
+import com.googlesource.gerrit.plugins.simplesubmitrules.config.SubmitConfig.LabelDefinition;
+import java.util.Collection;
+import java.util.Map;
+
+public final class ConfigTranslator {
+  private final PluginConfigFactory pluginConfigFactory;
+
+  protected static final String COPY_MIN_SCORE = "copy-min-score";
+  protected static final String COPY_MAX_SCORE = "copy-max-score";
+  protected static final String COPY_ALL_SCORES_IF_NO_CHANGE = "copy-all-scores-if-no-change";
+  protected static final String COPY_ALL_SCORES_IF_NO_CODE_CHANGE =
+      "copy-all-scores-if-no-code-change";
+  protected static final String COPY_ALL_SCORES_ON_MERGE_COMMIT_FIRST_PARENT_UPDATE =
+      "copy-all-scores-on-merge-commit-first-parent-update";
+  protected static final String COPY_ALL_SCORES_ON_TRIVIAL_REBASE =
+      "copy-all-scores-on-trivial-rebase";
+
+  @Inject
+  public ConfigTranslator(PluginConfigFactory pluginConfigFactory) {
+    this.pluginConfigFactory = pluginConfigFactory;
+  }
+
+  static void extractLabelCopyScores(LabelType labelType, LabelDefinition labelDefinition) {
+    if (labelType.isCopyMinScore()) {
+      labelDefinition.copyScores.add(COPY_MIN_SCORE);
+    }
+    if (labelType.isCopyMaxScore()) {
+      labelDefinition.copyScores.add(COPY_MAX_SCORE);
+    }
+    if (labelType.isCopyAllScoresIfNoChange()) {
+      labelDefinition.copyScores.add(COPY_ALL_SCORES_IF_NO_CHANGE);
+    }
+    if (labelType.isCopyAllScoresIfNoCodeChange()) {
+      labelDefinition.copyScores.add(COPY_ALL_SCORES_IF_NO_CODE_CHANGE);
+    }
+    if (labelType.isCopyAllScoresOnMergeFirstParentUpdate()) {
+      labelDefinition.copyScores.add(COPY_ALL_SCORES_ON_MERGE_COMMIT_FIRST_PARENT_UPDATE);
+    }
+    if (labelType.isCopyAllScoresOnTrivialRebase()) {
+      labelDefinition.copyScores.add(COPY_ALL_SCORES_ON_TRIVIAL_REBASE);
+    }
+  }
+
+  static void applyCopyScoresTo(Collection<String> copyScores, LabelType labelType) {
+    labelType.setCopyMinScore(copyScores.contains(COPY_MIN_SCORE));
+
+    labelType.setCopyMaxScore(copyScores.contains(COPY_MAX_SCORE));
+
+    labelType.setCopyAllScoresIfNoChange(copyScores.contains(COPY_ALL_SCORES_IF_NO_CHANGE));
+
+    labelType.setCopyAllScoresIfNoCodeChange(
+        copyScores.contains(COPY_ALL_SCORES_IF_NO_CODE_CHANGE));
+
+    labelType.setCopyAllScoresOnMergeFirstParentUpdate(
+        copyScores.contains(COPY_ALL_SCORES_ON_MERGE_COMMIT_FIRST_PARENT_UPDATE));
+
+    labelType.setCopyAllScoresOnTrivialRebase(
+        copyScores.contains(COPY_ALL_SCORES_ON_TRIVIAL_REBASE));
+  }
+
+  SubmitConfig convertFrom(ProjectState projectState) {
+    SubmitConfig submitConfig = new SubmitConfig();
+    PluginConfig config =
+        pluginConfigFactory.getFromProjectConfigWithInheritance(
+            projectState, Constants.PLUGIN_NAME);
+
+    submitConfig.comments.blockIfUnresolvedComments =
+        config.getBoolean(Constants.BLOCK_IF_UNRESOLVED_COMMENTS, false);
+
+    projectState
+        .getLabelTypes()
+        .getLabelTypes()
+        .forEach(
+            labelType -> {
+              extractLabelSettings(labelType, submitConfig);
+            });
+
+    return submitConfig;
+  }
+
+  void applyTo(SubmitConfig inConfig, ProjectState projectState) throws BadRequestException {
+    PluginConfig pluginConfig =
+        pluginConfigFactory.getFromProjectConfig(projectState, Constants.PLUGIN_NAME);
+    applyCommentRulesTo(inConfig.comments, pluginConfig);
+    applyLabelsTo(inConfig.labels, projectState.getLabelTypes());
+  }
+
+  private static void applyLabelsTo(Map<String, LabelDefinition> labels, LabelTypes config)
+      throws BadRequestException {
+    for (Map.Entry<String, LabelDefinition> entry : labels.entrySet()) {
+      String label = entry.getKey();
+      LabelDefinition definition = entry.getValue();
+      LabelType labelType = config.byLabel(label);
+
+      if (labelType == null) {
+        throw new BadRequestException(
+            "The label " + label + " does not exist. You can't change its config.");
+      }
+
+      definition.getFunction().ifPresent(labelType::setFunction);
+
+      applyCopyScoresTo(definition.copyScores, labelType);
+    }
+  }
+
+  private static void extractLabelSettings(LabelType labelType, SubmitConfig config) {
+    if (labelType == null) {
+      return;
+    }
+
+    SubmitConfig.LabelDefinition labelDefinition = new LabelDefinition();
+    config.labels.put(labelType.getName(), labelDefinition);
+
+    labelDefinition.function = labelType.getFunction().getFunctionName();
+    extractLabelCopyScores(labelType, labelDefinition);
+  }
+
+  private static void applyCommentRulesTo(CommentsRules comments, PluginConfig config) {
+    config.setBoolean(Constants.BLOCK_IF_UNRESOLVED_COMMENTS, comments.blockIfUnresolvedComments);
+  }
+}
