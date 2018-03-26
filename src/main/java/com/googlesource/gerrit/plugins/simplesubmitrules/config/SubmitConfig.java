d0cc4229905ae5f3c3ac9b0f3f2123005bf49ed3
Maxime Guerreiro <maximeg@google.com>
2018-03-26 15:15:13 +0200

    Add first version of the simple-submit-rules plugin
    This version contains the REST API, and two rules: block when unresolved
    comments are present, and ignore approval from the change author.
    
    Change-Id: I0167ffa57e27b10d5e977ef2684f20759531ccc6

---
+++ src/main/java/com/googlesource/gerrit/plugins/simplesubmitrules/config/SubmitConfig.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.simplesubmitrules.config;
+
+import com.google.common.base.MoreObjects;
+import com.google.gerrit.common.data.LabelFunction;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class SubmitConfig {
+  public Map<String, LabelDefinition> labels = new HashMap<>();
+  public CommentsRules comments = new CommentsRules();
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("labels", labels)
+        .add("comments", comments)
+        .toString();
+  }
+
+  static class LabelDefinition {
+    String function = null;
+    List<Integer> approvers = new ArrayList<>();
+    boolean ignoreSelfApproval = false;
+    List<String> copyScores = new ArrayList<>();
+
+    Optional<LabelFunction> getFunction() {
+      return LabelFunction.parse(function);
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("function", function)
+          .add("approvers", approvers)
+          .add("ignoreSelfApproval", ignoreSelfApproval)
+          .add("copyScores", copyScores)
+          .toString();
+    }
+  }
+
+  class CommentsRules {
+    boolean blockIfUnresolvedComments = false;
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("blockIfUnresolvedComments", blockIfUnresolvedComments)
+          .toString();
+    }
+  }
+}
