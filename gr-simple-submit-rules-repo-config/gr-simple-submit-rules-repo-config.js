c9f4060fd4071ae2477536677c31d6e943bee118
Maxime Guerreiro <maximeg@google.com>
2018-05-30 14:23:43 +0000

    Add a web interface to configure the comments rule
    Currently, only the "Block when unresolved comments are present" option
    is present. Other settings will be added at a later point.
    
    Change-Id: I95702d612b9dd45ca801d63dc380ef99e6320821

---
+++ gr-simple-submit-rules-repo-config/gr-simple-submit-rules-repo-config.js
@@ -0,0 +1,112 @@
+/**
+ * @license
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function () {
+  'use strict';
+  Polymer({
+    is: 'gr-simple-submit-rules-repo-config',
+
+    properties: {
+      repoName: String,
+      /** @type {?} */
+      _repoConfig: Object,
+      _configChanged: {
+        type: Boolean,
+        value: false,
+      },
+      _readOnly: {
+        type: Boolean,
+        value: true,
+      },
+    },
+
+    observers: [
+      '_handleConfigChanged(_repoConfig.*)',
+    ],
+
+    attached() {
+      this._loadRepo();
+    },
+
+    _isLoading() {
+      return this._loading || this._loading === undefined;
+    },
+
+    _handleConfigChanged() {
+      if (this._isLoading()) { return; }
+      this._configChanged = true;
+    },
+
+    _computeButtonDisabled(readOnly, configChanged) {
+      return readOnly || !configChanged;
+    },
+
+    _loadRepo() {
+      this.repoConfig = {};
+      if (!this.repoName) { return Promise.resolve(); }
+      const promises = [];
+
+      promises.push(this.$.restAPI.getLoggedIn().then(loggedIn => {
+        this._loggedIn = loggedIn;
+        if (loggedIn) {
+          this.$.restAPI.getRepoAccess(this.repoName).then(access => {
+            if (!access) { return Promise.resolve(); }
+
+            // If the user is not an owner, is_owner is not a property.
+            this._readOnly = !access[this.repoName].is_owner;
+          });
+        }
+      }));
+
+      promises.push(new GrPluginRestApi().get(this._endpointurl())
+        .then(config => {
+          if (!config) { return Promise.resolve(); }
+          this._repoConfig = config;
+          this._loading = false;
+        }));
+
+      return Promise.all(promises);
+    },
+
+    _formatRepoConfigForSave(repoConfig) {
+      return repoConfig;
+    },
+
+    _endpointurl() {
+      return "/projects/" + encodeURIComponent(this.repoName) + "/simple-submit-rules";
+    },
+
+    _handleSaveRepoConfig() {
+      this._loading = true;
+      return new GrPluginRestApi().put(this._endpointurl(), this._repoConfig)
+        .then(config => {
+          if (!config) { return Promise.resolve(); }
+
+          this.dispatchEvent(new CustomEvent('show-alert', {
+            detail: {
+              message: "Simple submit rules: configuration updated."
+            },
+            bubbles: true
+          }));
+
+          this._repoConfig = config;
+          this._loading = false;
+          this._configChanged = false;
+        })
+    },
+  });
+})();
